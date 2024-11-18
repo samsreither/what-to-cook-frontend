2104467a6bb5064a57d561c413cb681b6782f06b
Sam Reither <samsreither@gmail.com>
2024-11-18 16:36:14 -0500

    edits to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,5 @@ export default defineConfig({
   server: {     
     port: 3000, // Change the port number to 3000
   },
-})
+  base:'/what-to-cook-frontend/'
+});
